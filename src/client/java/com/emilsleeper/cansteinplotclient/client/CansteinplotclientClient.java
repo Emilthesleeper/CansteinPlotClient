dab1f69575a71b977544bab652e666b77e548812
Emilthesleeper <emilwege@icloud.com>
2026-05-14 11:32:40 +0200

    Initial commit

---
+++ src/client/java/com/emilsleeper/cansteinplotclient/client/CansteinplotclientClient.java
@@ -0,0 +1,10 @@
+package com.emilsleeper.cansteinplotclient.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CansteinplotclientClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/com/emilsleeper/cansteinplotclient/Cansteinplotclient.java
@@ -0,0 +1,10 @@
+package com.emilsleeper.cansteinplotclient;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Cansteinplotclient implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
